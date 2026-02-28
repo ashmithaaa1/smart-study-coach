5bf56570050f4abf40bf4c1e72be9f409e9bcaf0
ASHMITRHA MADAVARAM <ashmitrhamadavaram@ASHMITRHAs-MacBook-Air.local>
2026-03-01 01:14:27 +0530

    new update

--- script.js
+++ script.js
@@ -98,3 +98,37 @@ function generateMotivation() {
 
 renderTasks();
 updateTimer();
+document.getElementById("focusBtn").addEventListener("click", function () {
+    document.body.classList.toggle("focus-active");
+});
+let xp = 0;
+let streak = 0;
+
+document.getElementById("addXP").addEventListener("click", function () {
+    xp += 10;
+    streak += 1;
+
+    document.getElementById("xpCount").innerText = xp;
+    document.getElementById("streakCount").innerText = streak;
+});
+let progress = 0;
+
+document.getElementById("increaseProgress").addEventListener("click", function () {
+    if (progress < 100) {
+        progress += 10;
+        document.getElementById("progressBar").style.width = progress + "%";
+    }
+});
+document.getElementById("generatePlan").addEventListener("click", function () {
+    let subject = document.getElementById("subjectInput").value;
+
+    let plan = "📚 Study " + subject + " for 25 mins\n" +
+               "☕ Take 5 min break\n" +
+               "📝 Revise key concepts\n" +
+               "🧠 Practice 5 problems";
+
+    document.getElementById("studyPlan").innerText = plan;
+});
+
+
+
